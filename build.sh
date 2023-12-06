446ea5dfdd767e5105ae43abc081957061aaa221
rqil <122239746+rqil@users.noreply.github.com>
2023-12-06 10:24:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -124,7 +124,7 @@ BuildDev() {
     export GOARCH=${os_arch##*-}
     export CC=${cgo_cc}
     export CGO_ENABLED=1
-    go -buildmode=c-shared -o ./build/$appName-$os_arch -ldflags="$muslflags" -tags=jsoniter .
+    go build -buildmode=c-shared -o ./build/$appName-$os_arch -ldflags="$muslflags" -tags=jsoniter .
   done
 }
 
